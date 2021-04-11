73ceb4e1ad268cd6b8ffe1500ff05ccedefd35ea
Lee <leon.lee2050@gmail.com>
2021-04-11 12:01:36 -0400

    create observable from dom events

--- index.js
+++ index.js
@@ -1,45 +1,19 @@
-import {of, Observable} from 'rxjs';
+import { fromEvent } from 'rxjs';
 
-/*
- * Any code samples you want to play with can go in this file.
- * Updates will trigger a live reload on http://localhost:1234/
- * after running npm start.
- */
 const observer = {
-    next: value => console.log('next', value),
-    error: error => console.log('error', error),
-    complete: () => console.log('complete!')
+    next:val=>console.log('next',val),
+    error:error=>console.log('error',error),
+    complete:()=>console.log('complete'),
 }
 
 
-const observable = new Observable(subscriber => {
-    let count = 0;
+const source$ = fromEvent(document,'keyup');
 
-    const id = setInterval(()=>{
-        subscriber.next(count);
-        count +=1;
-
-    },1000);
-
-
-
-    return ()=>{
-        console.log("called");
-        clearInterval(id);
-    }
-});
-
-console.log("Before");
-let subscription = observable.subscribe(observer);
-let subscription2 = observable.subscribe(observer);
-
-subscription.add(subscription2);
+const subOne = source$.subscribe(observer);
+const subTwo = source$.subscribe(observer);
 
 setTimeout(()=>{
-    subscription.unsubscribe();
-},3500);
-
-console.log("after");
-
-//of('Hello', 'RxJS').subscribe(console.log);
+    console.log('unSubscribe');
+    subOne.unsubscribe();
+},3000)
 
