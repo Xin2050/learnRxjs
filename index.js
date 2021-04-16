876e858115aab0ede266155da97fd4c33870a792
Lee <leon.lee2050@gmail.com>
2021-04-16 16:57:14 -0400

    distinceUntilChanged

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
-import {of,fromEvent,interval} from 'rxjs';
-import {map, mapTo, pluck, filter, takeUntil, scan, takeWhile, tap} from 'rxjs/operators';
+import {of,fromEvent,interval,from} from 'rxjs';
+import {
+    map,
+    mapTo,
+    pluck,
+    filter,
+    takeUntil,
+    scan,
+    takeWhile,
+    tap,
+    distinctUntilChanged,
+    distinctUntilKeyChanged
+} from 'rxjs/operators';
 
 
-const counter$ = interval(1000);
-const abortButton = document.getElementById("abort");
-const abortClick$ = fromEvent(abortButton,'click');
 
-counter$.pipe(
-    mapTo(-1),
-    scan((accumulator,current)=>{
-        return accumulator+current;
-    },100),
-    takeUntil(abortClick$),
-    takeWhile(value=>value>=0),
-    //tap(console.log),
-).subscribe({
-    next:value=>{
-        document.getElementById("countdown").innerHTML = value;
-    },
-    complete:()=>{
-        document.getElementById("message").innerHTML = "complete";
-    }
-})
+const numbers = [1,2,3,4,5];
+
+const users = [
+    {name:'Brian',logged:false,token:null},
+    {name:'Brian',logged:false,token:null},
+    {name:'Xax',logged:true,token:"asdfsdlfksjdflkj"},
+    {name:'Leon',logged:true,token:"asdfsdlfksjdflkj"},
+]
+
+const state$ = from(users).pipe(
+    scan((accumulator,currentValue)=>{
+        return {...accumulator,...currentValue}
+    },{})
+
+);
+
+const name$ = state$.pipe(
+    distinctUntilKeyChanged('name'),
+    map(state=>state.name),
+);
+
+name$.subscribe(console.log);
