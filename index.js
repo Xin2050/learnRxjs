1ef45cb853fd971677370a527f6d3005541ef28a
Lee <leon.lee2050@gmail.com>
2021-04-16 17:45:57 -0400

    done

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-import {of,fromEvent,asyncScheduler} from 'rxjs';
-import {map, mapTo, pluck, filter, throttleTime, tap} from 'rxjs/operators';
+import {of, fromEvent, asyncScheduler, interval} from 'rxjs';
+import {map, mapTo, pluck, filter, throttleTime, tap, sampleTime, sample,auditTime,audit} from 'rxjs/operators';
 
-function calculateScrollPercent(element) {
+// function calculateScrollPercent(element) {
+//
+//     const {scrollTop,scrollHeight,clientHeight} = element;
+//     return (scrollTop/(scrollHeight-clientHeight)) *100
+// }
+//
+// const progressBar = document.querySelector('.progress-bar');
+//
+// const scroll$ = fromEvent(document,'scroll');
+// const progress$ = scroll$.pipe(
+//     map(({target})=>calculateScrollPercent(target.scrollingElement)),
+//     tap(console.log),
+//     throttleTime(30,asyncScheduler,{
+//         leading:false,
+//         trailing:true
+//     }),
+// )
+//
+// progress$.subscribe(percent=>{
+//     progressBar.style.width = `${percent}%`;
+// });
+//
 
-    const {scrollTop,scrollHeight,clientHeight} = element;
-    return (scrollTop/(scrollHeight-clientHeight)) *100
-}
+const click$ = fromEvent(document,'click');
 
-const progressBar = document.querySelector('.progress-bar');
-
-const scroll$ = fromEvent(document,'scroll');
-const progress$ = scroll$.pipe(
-    map(({target})=>calculateScrollPercent(target.scrollingElement)),
-    tap(console.log),
-    throttleTime(30,asyncScheduler,{
-        leading:false,
-        trailing:true
-    }),
-)
-
-progress$.subscribe(percent=>{
-    progressBar.style.width = `${percent}%`;
-});
-
-
+click$.pipe(
+    auditTime(3000),
+).subscribe(console.log)
