17c9248bf7f239f8f9faaf76ba0d51874f9a81e8
Lee <leon.lee2050@gmail.com>
2021-04-16 12:24:08 -0400

    progress bar

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-import {interval,timer} from 'rxjs';
+import {of,fromEvent} from 'rxjs';
+import {map, mapTo, pluck,filter} from 'rxjs/operators';
 
+function calculateScrollPercent(element) {
 
-const timer$ = timer(2000); // interval(1000)
+    const {scrollTop,scrollHeight,clientHeight} = element;
+    return (scrollTop/(scrollHeight-clientHeight)) *100
+}
 
+const progressBar = document.querySelector('.progress-bar');
 
-timer$.subscribe(console.log);
+const scroll$ = fromEvent(document,'scroll');
+const progress$ = scroll$.pipe(
+    map(({target})=>calculateScrollPercent(target.scrollingElement))
+)
 
+progress$.subscribe(percent=>{
+    progressBar.style.width = `${percent}%`;
+});
 
 
 
