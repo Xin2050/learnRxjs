340607cfdd28978ad879fa11e7b410cf1bd5cf6e
Lee <leon.lee2050@gmail.com>
2021-04-16 17:46:48 -0400

    done

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 import {of,fromEvent,asyncScheduler} from 'rxjs';
-import {map, mapTo, pluck, filter, throttleTime, tap} from 'rxjs/operators';
+import {map, mapTo, pluck, filter, throttleTime, tap, sampleTime} from 'rxjs/operators';
 
-function calculateScrollPercent(element) {
+const click$ = fromEvent(document,'click');
 
-    const {scrollTop,scrollHeight,clientHeight} = element;
-    return (scrollTop/(scrollHeight-clientHeight)) *100
-}
+click$.pipe(
+    sampleTime(2000), //每2秒把之前间隙里的事件发送出来
+    map(({clientX,clientY})=>({clientX,clientY})),
 
-const progressBar = document.querySelector('.progress-bar');
-
-const scroll$ = fromEvent(document,'scroll');
-const progress$ = scroll$.pipe(
-    map(({target})=>calculateScrollPercent(target.scrollingElement)),
-    tap(console.log),
-    throttleTime(30,asyncScheduler,{
-        leading:false,
-        trailing:true
-    }),
-)
-
-progress$.subscribe(percent=>{
-    progressBar.style.width = `${percent}%`;
-});
-
+).subscribe(console.log)
 
