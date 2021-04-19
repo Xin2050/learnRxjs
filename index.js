d76b6951b82069b901d938d00860a390780d8116
Lee <leon.lee2050@gmail.com>
2021-04-19 17:56:33 -0400

    exhaustMap

--- index.js
+++ index.js
@@ -11,26 +11,34 @@ import {
     distinctUntilChanged,
     distinctUntilKeyChanged,
     debounceTime,
-    debounce, switchMap, concatMap, take, delay,
+    debounce, switchMap, concatMap,exhaustMap, take, delay,
 } from 'rxjs/operators';
 import {ajax} from "rxjs/ajax";
 
-// const clicks$ = fromEvent(document,'click');
-// const interval$ = interval(1000);
-//
-// clicks$.pipe(
-//     concatMap(()=>interval$.pipe(take(3))),
-//
-// ).subscribe(console.log)
-const saveAnswer = answer => {
-    return of(`Saved ${answer}`).pipe(
-        delay(1500)
-    );
-}
-const radioButtons = document.querySelectorAll('.radio-option');
+const login$ = fromEvent(document.getElementById("login"),'click');
 
-const answerChange$ = fromEvent(radioButtons,'click');
-answerChange$.pipe(
-    concatMap(event=>saveAnswer(event.target.value)),
-).subscribe(console.log);
+const authenticateUser = ()=>{
+    return ajax.post(
+        'https://reqres.in/api/login',
+        {
+            email:"eve.holt@reqres.in",
+            password:'cityslicka'
+        }
+    )
+}
+
+login$.pipe(
+    exhaustMap(()=>authenticateUser()),
+).subscribe(console.log)
+// const saveAnswer = answer => {
+//     return of(`Saved ${answer}`).pipe(
+//         delay(1500)
+//     );
+// }
+// const radioButtons = document.querySelectorAll('.radio-option');
+//
+// const answerChange$ = fromEvent(radioButtons,'click');
+// answerChange$.pipe(
+//     exhaustMap(event=>saveAnswer(event.target.value)),
+// ).subscribe(console.log);
 
