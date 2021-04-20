4366272b84667fa34d9ebf7635e9f3afa79d7255
Lee <leon.lee2050@gmail.com>
2021-04-20 18:02:58 -0400

    concat

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-import {of,fromEvent,interval} from 'rxjs';
-import {map, mapTo, pluck,filter,scan,tap,take,first,takeWhile} from 'rxjs/operators';
+import {of, fromEvent, interval, EMPTY, empty} from 'rxjs';
+import {map, mapTo, pluck, filter,concat, scan, tap, take, first, takeWhile, delay, startWith} from 'rxjs/operators';
 
-const counter$ = interval(1000);
+const interval$ = interval(1000);
+const delayed$ = EMPTY.pipe(delay(1000))
 
-counter$.pipe(
-    mapTo(-1),
-    scan((accumulator,current)=>{
-        return accumulator+current;
-    },5),
-    takeWhile(value=>value>=0),
-    tap(console.log),
+
+delayed$.pipe(
+    concat(
+        delayed$.pipe(startWith("3...")),
+        delayed$.pipe(startWith("2...")),
+        delayed$.pipe(startWith("1...")),
+        delayed$.pipe(startWith("Go!")),
+    ),
+    startWith("Get Ready!"),
 ).subscribe(console.log)
