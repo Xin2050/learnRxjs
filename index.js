46f7b34607c534450989dd81a62d241f15f546b3
Lee <leon.lee2050@gmail.com>
2021-04-20 14:04:14 -0400

    startWith

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import {of,fromEvent,interval} from 'rxjs';
-import {map, mapTo, pluck, filter, takeUntil, scan, takeWhile, tap} from 'rxjs/operators';
+import {map, mapTo, pluck, filter, takeUntil, scan, takeWhile, tap, startWith,endWith,concatAll} from 'rxjs/operators';
+
+
+// const numbers$ = of(1,2,3);
+//
+// numbers$.pipe(
+//     startWith('a','b','c'),
+//     endWith('d','e','f'),
+// ).subscribe(console.log)
 
 
 const counter$ = interval(1000);
 const abortButton = document.getElementById("abort");
 const abortClick$ = fromEvent(abortButton,'click');
 
+const COUNTDOWN_FROM = 10;
 counter$.pipe(
     mapTo(-1),
     scan((accumulator,current)=>{
         return accumulator+current;
-    },100),
+    },COUNTDOWN_FROM),
     takeUntil(abortClick$),
     takeWhile(value=>value>=0),
+    startWith(COUNTDOWN_FROM),
     //tap(console.log),
 ).subscribe({
     next:value=>{
