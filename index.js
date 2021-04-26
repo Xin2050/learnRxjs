0ad772664007ba72d5161b876c8947566018aa2e
Lee <leon.lee2050@gmail.com>
2021-04-25 20:28:43 -0400

    set master to init

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
 import {of,fromEvent} from 'rxjs';
 import {map, mapTo, pluck,filter} from 'rxjs/operators';
 
-function calculateScrollPercent(element) {
-
-    const {scrollTop,scrollHeight,clientHeight} = element;
-    return (scrollTop/(scrollHeight-clientHeight)) *100
-}
-
-const progressBar = document.querySelector('.progress-bar');
-
-const scroll$ = fromEvent(document,'scroll');
-const progress$ = scroll$.pipe(
-    map(({target})=>calculateScrollPercent(target.scrollingElement))
-)
-
-progress$.subscribe(percent=>{
-    progressBar.style.width = `${percent}%`;
-});
 
 
 
