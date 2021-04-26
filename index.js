85c070117e2dc82849d0bc70aa97e005610c1e22
Lee <leon.lee2050@gmail.com>
2021-04-25 20:50:32 -0400

    subject init

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-import {of,fromEvent} from 'rxjs';
-import {map, mapTo, pluck,filter} from 'rxjs/operators';
+import {of, fromEvent, Subject, interval} from 'rxjs';
+import {map, mapTo, pluck, filter, tap} from 'rxjs/operators';
+
+const observer = {
+    next: val => console.log('next', val),
+    error: err => console.log('error', err),
+    complete: () => console.log('complete')
+};
 
 
 
+const subject$ = new Subject();
+
+const subscription$ = subject$.subscribe(observer);
+
+
+
+const subscription$2 = subject$.subscribe(observer);
+
+
+
+const interval$ = interval(2000).pipe(
+    tap(val=>console.log("new interval",val)),
+)
+
+
+interval$.subscribe(subject$);
 
