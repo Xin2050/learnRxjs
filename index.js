be695010225a2d490257074688f0e5f64fe75a65
Lee <leon.lee2050@gmail.com>
2021-04-27 12:54:20 -0400

    queueScheduler

--- index.js
+++ index.js
@@ -9,24 +9,20 @@ import {
     timer
 } from 'rxjs';
 import {multicast, observeOn, refCount, share, tap} from "rxjs/operators";
-import {takeWhile} from "rxjs/src/internal/operators/takeWhile";
+
 
 const observer ={
     next: val=> console.log('next',val),
     error: err=> console.log('error',err),
     complete: ()=> console.log('complete'),
 }
-console.log("ok");
-const ball = document.getElementById('ball');
-// animationFrameScheduler.schedule(function(position){
-//   ball.style.transform = `translate3d(0, ${position}px, 0`;
-//   if(position <= 200) {
-//     this.schedule(position + 1)
-//   }
-// }, 0, 0);
 
-interval(0,animationFrameScheduler).pipe(
-    takeWhile(val=>val<=200),
-).subscribe(val=>{
-    ball.style.transform = `translate3d(0, ${val}px, 0`;
+queueScheduler.schedule(() => {
+    queueScheduler.schedule(() => {
+        console.log('inside second queue');
+        queueScheduler.schedule(() => {
+            console.log('inside third queue');
+        });
+    });
+    console.log('inside first queue');
 })
