23e00c113ab4824451bbbc1dc6186976364eb43f
Lee <leon.lee2050@gmail.com>
2021-04-27 11:47:01 -0400

    ball move

--- index.js
+++ index.js
@@ -9,15 +9,24 @@ import {
     timer
 } from 'rxjs';
 import {multicast, observeOn, refCount, share, tap} from "rxjs/operators";
+import {takeWhile} from "rxjs/src/internal/operators/takeWhile";
 
 const observer ={
     next: val=> console.log('next',val),
     error: err=> console.log('error',err),
     complete: ()=> console.log('complete'),
 }
+console.log("ok");
+const ball = document.getElementById('ball');
+// animationFrameScheduler.schedule(function(position){
+//   ball.style.transform = `translate3d(0, ${position}px, 0`;
+//   if(position <= 200) {
+//     this.schedule(position + 1)
+//   }
+// }, 0, 0);
 
-asyncScheduler.schedule(console.log,0,"AsyncScheduler");  //3
-asapScheduler.schedule(console.log,0,"AsapScheduler"); //1
-Promise.resolve('from promise').then(console.log) //2
-
-console.log("synchronous console.log")
+interval(0,animationFrameScheduler).pipe(
+    takeWhile(val=>val<=200),
+).subscribe(val=>{
+    ball.style.transform = `translate3d(0, ${val}px, 0`;
+})
