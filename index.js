1e7e39177c56926b35065550e538e3a60dfdd87e
Lee <leon.lee2050@gmail.com>
2021-04-26 23:33:35 -0400

    store

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-import {of,fromEvent} from 'rxjs';
-import {map, mapTo, pluck,filter} from 'rxjs/operators';
+import {of, fromEvent, BehaviorSubject, Subject} from 'rxjs';
+import {map, mapTo, pluck, filter, scan, distinctUntilKeyChanged} from 'rxjs/operators';
 
 
-import {ObservableStore} from './store';
+export class ObservableStore {
 
+    constructor(initialState) {
+        this._store = new BehaviorSubject(
+            initialState
+        )
+        this._stateUpdate = new Subject();
+        this._stateUpdate.pipe(
+            scan((acc,curr)=>{
+                return {...acc,...curr}
+            },initialState)
+        ).subscribe(this._store)
+    }
+
+    updateState(stateUpdate) {
+        this._stateUpdate.next(stateUpdate);
+    }
+
+    selectState(stateKey) {
+        return this._store.pipe(
+            distinctUntilKeyChanged(stateKey),
+            pluck(stateKey)
+        )
+    }
+    stateChanges() {
+        return this._store.asObservable()
+    }
+
+}
 
 const store = new ObservableStore({
     user:'brian',
--- store.js
+++ store.js
@@ -1,32 +1,3 @@
 import {BehaviorSubject, Subject} from "rxjs";
 import {distinctUntilKeyChanged, pluck, scan} from "rxjs/operators";
 
-export class ObservableStore {
-
-    constructor(initialState) {
-        this._store = new BehaviorSubject(
-            initialState
-        )
-        this._stateUpdate = new Subject();
-        this._stateUpdate.pipe(
-            scan((acc,curr)=>{
-               return {...acc,...curr}
-            },initialState)
-        ).subscribe(this._store)
-    }
-
-    updateState(stateUpdate) {
-        this._stateUpdate.next(stateUpdate);
-    }
-
-    selectState(stateKey) {
-        return this._store.pipe(
-            distinctUntilKeyChanged(stateKey),
-            pluck(stateKey)
-        )
-    }
-    stateChanges() {
-        return this._store.asObservable()
-    }
-
-}
